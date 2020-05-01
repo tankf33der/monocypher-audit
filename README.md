d5ab14bf8e6f231fa001471600b6cdda07e3461a
Mike <tankf33der@disroot.org>
2020-05-01 10:21:41 +0300

    .

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Lets create something usefull to use Frama-C and CompCert over Monocypher.
+
+```
+$ frama-c -eva -eva-precision 11 -eva-builtins-auto -memexec-all -no-val-show-progress -main p1305 *.c
+```
--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "monocypher.h"
+
+#define ARRAY(name, size) \
+    uint8_t name[size]; \
+    for(size_t i = 0; i < size; i++) name[i] = 123;
 
 void p1305(void) {
-
+    ARRAY(mac, 16);
+    ARRAY(key, 32);
+    for(size_t i = 0; i < 32; i++) {
+        crypto_poly1305(mac, key, i, key);
+    }
 }
 
 int main(void) {
