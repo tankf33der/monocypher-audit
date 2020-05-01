e4819c0eab09e6abdc28927d50e81de3fafec356
Mike <tankf33der@disroot.org>
2020-05-01 10:30:17 +0300

    .

--- main.c
+++ main.c
@@ -2,19 +2,27 @@
 #include <stdio.h>
 #include "monocypher.h"
 
+typedef uint8_t u8;
+
 #define ARRAY(name, size) \
-    uint8_t name[size]; \
+    u8 name[size]; \
     for(size_t i = 0; i < size; i++) name[i] = 123;
 
 void p1305(void) {
     ARRAY(mac, 16);
     ARRAY(key, 32);
-    for(size_t i = 0; i < 32; i++) {
-        crypto_poly1305(mac, key, i, key);
+    ARRAY(in,  44);
+    for(size_t i = 0; i < 44; i++) {
+        crypto_poly1305(mac, in, i, key);
     }
+}
+
+void blake2b(void) {
+
 }
 
 int main(void) {
     p1305();
+    blake2b();
     return 0;
 }
